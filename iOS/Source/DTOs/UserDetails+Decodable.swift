21f51324509573f5fb47ae08f0fec5ee24f4b52d
Alessandro Limardo <alessandro.limardo@kaleyra.com>
2024-11-15 11:11:54 +0100

    Add tests for UserDetail decode behaviour

--- iOS/Source/DTOs/UserDetails+Decodable.swift
+++ iOS/Source/DTOs/UserDetails+Decodable.swift
@@ -6,14 +6,14 @@ import Foundation
 extension UserDetails: Decodable {
 
     private enum CodingKeys: String, CodingKey {
-        case userId
+        case userID
         case name
         case imageUrl
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.userID = try container.decode(String.self, forKey: .userId)
+        self.userID = try container.decode(String.self, forKey: .userID)
         self.name = try container.decodeIfPresent(String.self, forKey: .name)
         self.imageURL = try container.decodeIfPresent(String.self, forKey: .imageUrl)
     }
